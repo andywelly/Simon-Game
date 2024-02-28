5f478531520b08a7240b31fde2870af2d23fe756
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-02-28 17:46:16 +1100

    Added sequence check function

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var started = false;
+
+var level = 0;
+
+// On button click
 
 $(".btn").on("click", function() { 
     var pressedColour = $(this).attr("id");
@@ -12,8 +17,38 @@ $(".btn").on("click", function() {
 
     playSound(pressedColour);
     animatePress(pressedColour);
+    checkSequence(userClickedPattern.length -1);
 
 });
+
+// On Keypress
+
+$(document).on("keypress", function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function checkSequence(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        console.log("wrong");
+        $("body").addClass("game-over");
+    }
+
+}
+
+
 
 function playSound(currentColour) {
     var audio = new Audio('./sounds/' + currentColour + '.mp3');
@@ -21,6 +56,12 @@ function playSound(currentColour) {
 }
 
 function nextSequence() {
+
+    userClickedPattern = [];
+
+    level ++;
+    $("#level-title").text("Level " + level);
+
     var randNum = Math.floor(Math.random() * 4);
 
     var randColour = colours[randNum];
