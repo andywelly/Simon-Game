e93900463933ada17a61fc2e959a725df8a55567
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-02-28 16:37:58 +1100

    Added button behaviour

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-alert("Hello");
+var colours = ["green", "red", "yellow", "blue"];
+
+var gamePattern = [];
+
+function nextSequence() {
+    var randNum = Math.floor(Math.random() * 4);
+    gamePattern.push(colours[randNum]);
+};
